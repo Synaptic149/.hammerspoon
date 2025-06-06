d6fb86de03baa9227cde5f722057958035347c0d
Synaptic <devkrishnasahay@gmail.com>
2025-06-06 11:55:07 -0700

    Initial commit

---
+++ init.lua
@@ -0,0 +1,40 @@
+-- Track current state
+local currentMotionSetting = nil
+local currentTransparencySetting = nil
+local currentPowerSource = nil
+
+function updateAccessibilitySettings()
+  local powerSource = hs.battery.powerSource()
+  if powerSource == currentPowerSource then return end  -- no change, exit early
+
+  currentPowerSource = powerSource
+  local onBattery = (powerSource == "Battery Power")
+  local desiredMotion = onBattery
+  local desiredTransparency = onBattery
+
+  local changed = false
+
+  if currentMotionSetting ~= desiredMotion then
+    hs.execute("defaults write com.apple.universalaccess reduceMotion -bool " .. tostring(desiredMotion), true)
+    currentMotionSetting = desiredMotion
+    changed = true
+  end
+
+  if currentTransparencySetting ~= desiredTransparency then
+    hs.execute("defaults write com.apple.universalaccess reduceTransparency -bool " .. tostring(desiredTransparency), true)
+    currentTransparencySetting = desiredTransparency
+    changed = true
+  end
+
+  if changed then
+    hs.task.new("/usr/bin/killall", nil, function() return false end, { "Dock" }):start()
+    hs.alert.show("Running on " .. (onBattery and "battery" or "AC"))
+  end
+end
+
+-- Use older, compatible battery watcher
+powerWatcher = hs.battery.watcher.new(updateAccessibilitySettings)
+powerWatcher:start()
+
+-- Run once at startup
+updateAccessibilitySettings()
